30df77eb681b975b0f7cca66026ecfed38e9c06b
Michael Maloney <maloney.mc@gmail.com>
2010-11-27 17:01:10 -0500

    changing adv search routes, adding search results view

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ ActionController::Routing::Routes.draw do |map|
   map.sign_out '/sign_out', :controller => 'sessions', :action => 'destroy'
   
   map.resources :tracks
-  map.resources :searches
+  map.resources :searches, :only => [:create, :show]
   
   map.formatted_tracks 'tracks.:format', :controller => 'tracks', :action => 'index'
   
